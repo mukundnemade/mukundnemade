6d2f14d36d24d717637ecefef0056acd0e14766c
mukundnemade <39467310+mukundnemade@users.noreply.github.com>
2018-05-20 17:05:12 -0400

    Update config.js

--- bin/config.js
+++ bin/config.js
@@ -13,8 +13,8 @@ var autoconfig  = function (config_overrides){
   })
 
   var ws_info = {
-    id: "MBN-nationalparks-js",
-    displayName: "National Parks (JS)",
+    id: "nationalparks-js",
+    displayName: "MBN-National Parks (JS)",
     type: "cluster",
     center: {'latitude': '47.039304', 'longitude': '14.505178'},
     zoom: 4
